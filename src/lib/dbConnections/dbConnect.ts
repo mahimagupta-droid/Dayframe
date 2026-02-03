4bd62377634df183ac2386ae164581761cf7aa1c
Mahima Gupta <guptamahima583@gmail.com>
2026-02-03 15:21:32 +0530

    cleaning the code for build purposes

--- src/lib/dbConnections/dbConnect.ts
+++ src/lib/dbConnections/dbConnect.ts
@@ -1,31 +1,35 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from "mongoose";
-const mongoDB_URL= process.env.MONGODB_ATLAS_URL;
+const mongoDB_URL = process.env.MONGODB_ATLAS_URL || `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0.fx54vz0.mongodb.net/dayframe?appName=Cluster0`;
 
-if(!mongoDB_URL){
-    throw new Error("define database url in the .env file");
+interface MongooseCache {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
 }
 
-let cached = (global as any).mongoose;
+let cached: MongooseCache = (global as any).mongoose;
 
-if(!cached){
+if (!cached) {
     cached = (global as any).mongoose = {
         conn: null,
-        promise: null
+        promise: null,
     };
 }
 
 export async function dbConnect() {
-    if(cached.conn){
+    if (cached.conn) {
         return cached.conn;
     }
-    if(!cached.promise){
-        cached.promise = mongoose.connect(mongoDB_URL!).then((mongoose) => {
-            return mongoose
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+        cached.promise = mongoose.connect(mongoDB_URL, opts).then((m) => {
+            return m;
         });
     }
     try {
-        cached.conn = await cached.promise
+        cached.conn = await cached.promise;
     } catch (error) {
         cached.conn = null;
         throw error;
