4ec1abe71243d9fe6d07460a378be1d0671cdaad
Mahima Gupta <guptamahima583@gmail.com>
2026-02-10 01:52:49 +0530

    conditional rendering of login statement on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
-export default function Home() {
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const {userId} = await auth();
+  if(!userId){
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen text-4xl italic">
+       <div className="underline">
+         Dayframe -  Student Productivity and Life Manager
+       </div>
+        <div className="text-2xl mt-12 not-italic bg-slate-800 p-4 rounded-xl border-slate-800 opacity-80">Please login to continue</div>
+    </div>
+    )
+  }
   return (
     <div className="flex items-center justify-center min-h-screen text-4xl italic">
         Dayframe -  Student Productivity and Life Manager
