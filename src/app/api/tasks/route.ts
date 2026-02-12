067e45564c21176b7e2f5b9aeb83f665f2ba5a5a
Mahima Gupta <guptamahima583@gmail.com>
2026-02-13 00:20:16 +0530

    database and post tasks bugs fixed

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -10,16 +10,27 @@ export async function POST(request: NextRequest) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
     await dbConnect();
-    const {
-      title,
-      deadline,
-      priority,
-      description,
-      category,
-      difficulty,
-      timeRequired,
-      status,
-    } = await request.json();
+    const body = await request.json();
+
+    //check for valid date
+    const date = new Date(body.deadline);
+    if(isNaN(date.getTime())){
+        return NextResponse.json({ error: "Invalid date format" }, { status: 400 });
+    }
+
+    // Check if deadline is in the past
+    if (date < new Date(new Date().setHours(0,0,0,0))) {
+      return NextResponse.json({ error: "Deadline cannot be in the past." }, { status: 400 });
+    }
+
+    //check if any of the fields that have enums are empty strings, if they are, set them to undefined so that the default value is used in the schema
+    const fieldsToCheck = ['deadline', 'priority', 'category', 'difficulty', 'timeRequired', 'status']
+    fieldsToCheck.forEach((field) => {
+        if(body[field] === ""){
+            body[field] = undefined;
+        }
+    })
+    const { title, deadline, priority, description, category, difficulty, timeRequired, status } = body;
     const newTask = await Tasks.create({
       clerkId: userId,
       title,
--- src/app/profile-form/page.tsx
+++ src/app/profile-form/page.tsx
@@ -20,6 +20,8 @@ export default function ProfileForm() {
                 },
                 body: JSON.stringify(formData)
             });
+
+            
             toast.success("User profile created successfully!")
             handleFetch();
         } catch (error) {
