03ce35c885bc892e7cc1bf62d1bc515342b07ca6
Mahima Gupta <guptamahima583@gmail.com>
2026-02-14 02:39:01 +0530

    adding home page link to logo

--- src/app/separate-components/navigation.tsx
+++ src/app/separate-components/navigation.tsx
@@ -5,11 +5,13 @@ export default async function Navigation() {
         <nav className="fixed top-0 z-50 h-16 w-full border-b bg-black text-white">
             <div className="mx-auto flex h-full items-center justify-between px-6">
 
-                <div className="flex items-center">
+                <Link href="/" className="cursor-pointer">
+                    <div className="flex items-center">
                     <span className="text-2xl font-semibold tracking-wide ml-4">
                         Dayframe
                     </span>
                 </div>
+                </Link>
 
                 <div className="flex items-center gap-6 mr-4">
                     <div className="hidden md:flex items-center gap-6">
