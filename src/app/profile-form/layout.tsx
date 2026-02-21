fb4415eca75ad0c8e2a9ac839d6c2b785a4e5737
Mahima Gupta <guptamahima583@gmail.com>
2026-02-21 20:28:41 +0530

    removing useless imports

--- src/app/profile-form/layout.tsx
+++ src/app/profile-form/layout.tsx
@@ -1,13 +1,6 @@
-import { DottedGlowBackground } from "@/components/ui/dotted-glow-background";
 export default function ProfileFormLayout({ children }: { children: React.ReactNode; }) {
   return (
     <div className="min-h-[calc(100vh-64px)] bg-black text-white">
-      <DottedGlowBackground
-                    glowColor="rgb(209, 255, 0)"      
-                    radius={3.5}                           
-                    opacity={1}                         
-                    speedScale={0.24}
-                  />
       {children}
     </div>
 
